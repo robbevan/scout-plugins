ec476d96e15ea673c392d51a257e47b600de1fa2
Rob Bevan <robbevan@xpt.com>
2009-05-27 13:30:46 +0100

    upgraded Resource Limits from Scout 1.x to 2.x

--- resource_limits/resource_limits.rb
+++ resource_limits/resource_limits.rb
@@ -1,21 +1,15 @@
 class ResourceLimits < Scout::Plugin
-  def run
+  def build_report
     zonename = `zonename`
-    max_rss = @options["max_rss"].to_i
-    max_swap = @options["max_swap"].to_i
+    max_rss = option("max_rss").to_i
+    max_swap = option("max_swap").to_i
     prstat_output = `prstat -Z -s rss 1 1`.grep(/#{zonename.chomp}/).first.gsub(/\s{2,}/, ' ').strip.split(" ")
-    report = {:report => Hash.new, :alerts => Array.new}
-    report[:report][:rss] = prstat_output[3].gsub(/M/, '').to_i
-    report[:report][:swap] = prstat_output[2].gsub(/M/, '').to_i
-    if report[:report][:rss] > max_rss
-      report[:alerts] << { :subject => "Max RSS Exceeded: #{report[:report][:rss]}(#{max_rss})" }
-    end
-    if report[:report][:swap] > max_swap
-      report[:alerts] << { :subject => "Max SWAP Exceeded: #{report[:report][:swap]}(#{max_swap})" }
-    end
-    report
-  rescue
-    { :error => { :subject => "Couldn't use `prstat` as expected.",
-                  :body    => $!.message } }
+    rss = prstat_output[3].gsub(/M/, '').to_i
+    swap = prstat_output[2].gsub(/M/, '').to_i
+    report(:rss => rss)
+    report(:swap => swap)
+    # removed alerts now handled by triggers
+  rescue => e
+    error("Couldn't use `prstat` as expected.", "#{e.message} ----- #{e.backtrace}")
   end
 end
